f10cea4bb6e6e6f75d42107b8a7cad9746c3e03f
Denis Zamataev <denis.zamataev@gmail.com>
2013-11-05 15:41:52 +0400

    Headers... are you kidding...

---
+++ Core/Source/Chivy.h
@@ -0,0 +1,17 @@
+//
+//  Chivy.h
+//  Chivy
+//
+//  Created by Denis Zamataev on 11/5/13.
+//  Copyright (c) 2013 Denis Zamataev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef _CHIVY_
+    #define _CHIVY_
+
+    #import "CHWebBrowserController.h"
+    #import "CHScrollingInspector.h"
+
+#endif /* _CHIVY_ */
